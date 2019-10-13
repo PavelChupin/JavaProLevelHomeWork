c78d759ff6a79658ce0c17fceff0989d6a7b85e3
Chupin Pevel <pavel.chupin@gmail.com>
2019-10-13 10:58:35 +0700

    Реализована ДЗ3

--- src/homework/lesson2/MyArrayDataException.java
+++ src/homework/lesson2/MyArrayDataException.java
@@ -3,7 +3,7 @@ package homework.lesson2;
 public class MyArrayDataException extends NumberFormatException{
 
     public MyArrayDataException(int y, int x, String r){
-        super("Ячейка массива: Y = " + y + ", X = " + x + ". Содержит значение с не числовыми символами или имеется переполнение типа данных: " + r + ".");
+        super(String.format("Ячейка массива: Y = %d, X = %d. Содержит значение с не числовыми символами или имеется переполнение типа данных: %s.",y,x,r));
 
     }
 
---
+++ src/homework/lesson3/MainHomeWorkLesson3_Task1.java
@@ -0,0 +1,28 @@
+package homework.lesson3;
+
+public class MainHomeWorkLesson3_Task1 {
+    public static void main(String[] args) {
+        String[] arrays = new String[10];
+        arrays[0] = "Two";
+        arrays[1] = "ret1";
+        arrays[2] = "Three";
+        arrays[3] = "ret2";
+        arrays[4] = "ret3";
+        arrays[5] = "Two";
+        arrays[6] = "Three";
+        arrays[7] = "ret4";
+        arrays[8] = "Three";
+        arrays[9] = "ret5";
+
+        Words words = new Words(arrays);
+
+        //Получаем уникальные
+        words.uniq().forEach((s)->{
+            System.out.println(s);
+        });
+
+        words.countWords().forEach((r,t)->{
+            System.out.println("Слово " + r + " встречается в массиве " + t + " раз");
+        });
+    }
+}
