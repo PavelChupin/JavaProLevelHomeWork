368f1626cfc579ad8475f0953ae11744b5bab1bb
Chupin Pevel <pavel.chupin@gmail.com>
2019-10-21 10:37:54 +0700

    Поправлены ошибки ДЗ5

--- src/main/java/homework/lesson5/ArrayClass.java
+++ src/main/java/homework/lesson5/ArrayClass.java
@@ -1,84 +1,85 @@
 package homework.lesson5;
 
+import java.util.LinkedList;
+
 public class ArrayClass {
 
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
     private float[] arr;
+    private float[] a1;
+    private float[] a2;
 
-    public ArrayClass(){
+    public ArrayClass() {
         this.arr = new float[SIZE];
         for (int i = 0; i < SIZE; i++) {
             arr[i] = 1f;
         }
     }
-    /*
-    private float[] creatArray() {
-        float[] arr = new float[SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            arr[i] = 1f;
+
+    private void array(float[] a) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (float)(a[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        return arr;
-    }*/
+    }
+
+    private void arrayA1(int index) {
+        for (int i = 0; i < a1.length; i++, index++) {
+            a1[i] = (float) (a1[i] * Math.sin(0.2f + index / 5) * Math.cos(0.2f + index / 5) * Math.cos(0.4f + index / 2));
+        }
+    }
+
+    private void arrayA2(int index) {
+        for (int i = 0; i < a2.length; i++, index++) {
+            a2[i] = (float) (a2[i] * Math.sin(0.2f + index / 5) * Math.cos(0.2f + index / 5) * Math.cos(0.4f + index / 2));
+        }
+    }
+
+    private float[] arrayHalf(int indexFrom, int indexTo) {
+        float[] f = new float[HALF];
+        System.arraycopy(arr, indexFrom, f, 0, indexTo);
+        return f;
+    }
+
+    private float[] arrayJoin() {
+        float[] f = new float[SIZE];
+        System.arraycopy(a1, 0, f, 0, HALF);
+        System.arraycopy(a2, 0, f, HALF, HALF);
+        return f;
+    }
 
     public float method1() {
         //Создаем массив
-        float[] array = this.arr;//creatArray();
+        float[] array = this.arr;
         long a = System.currentTimeMillis();
-        array(array,0);
+        array(array);
         return System.currentTimeMillis() - a;
     }
 
-    public float method2() {
-        //Создаем массив
-        float[] array = arr;
-        float[] a1 = new float[HALF];
-        float[] a2 = new float[HALF];
+    public float method2() throws InterruptedException {
         long a = System.currentTimeMillis();
-        //Разбиваем массивы на два
-        System.arraycopy(array, 0, a1, 0, HALF);
-        System.arraycopy(array, HALF, a2, 0, HALF);
 
-        new Thread(() -> array(a1,0));
-        new Thread(() -> array(a2,HALF));
+        this.a1 = arrayHalf(0, HALF);
+        this.a2 = arrayHalf(HALF, HALF);
 
-        //Склеиваем массиф
-        System.arraycopy(a1, 0, array, 0, HALF);
-        System.arraycopy(a2, 0, array, HALF, HALF);
+        Thread t1 = new Thread(() -> arrayA1(0));
+        Thread t2 = new Thread(() -> arrayA2(HALF));
+        t1.start();
+        t2.start();
 
-        return System.currentTimeMillis() - a;
-    }
+        //Делаем задержку, ждем пока все потоки закончат работать
+        do {
+            Thread.sleep(1);
+        } while (t1.isAlive() || t2.isAlive());
 
-    public float method3() {
-        //Создаем массив
-        float[] array = arr;
-        float[] a1 = new float[HALF];
-        float[] a2 = new float[HALF];
-        long a = System.currentTimeMillis();
-        //Разбиваем массивы на два
-        System.arraycopy(array, 0, a1, 0, HALF);
-        System.arraycopy(array, HALF, a2, 0, HALF);
-
-        new Thread(() -> arraysync(a1,0));
-        new Thread(() -> arraysync(a2, HALF));
-
-        //Склеиваем массиф
-        System.arraycopy(a1, 0, array, 0, HALF);
-        System.arraycopy(a2, 0, array, HALF, HALF);
+        //Склеиваем массив
+        arrayJoin();
 
         return System.currentTimeMillis() - a;
     }
 
-    private void array(float[] arr, int index) {
-        for (int i = 0; i < arr.length; i++, index++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + index / 5) * Math.cos(0.2f + index / 5) * Math.cos(0.4f + index / 2));
-        }
-    }
 
-    private synchronized void arraysync(float[] arr, int index) {
-        for (int i = 0; i < arr.length; i++, index++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + index / 5) * Math.cos(0.2f + index / 5) * Math.cos(0.4f + index / 2));
-        }
-    }
+
 
 }
+
--- src/main/java/homework/lesson5/MainHomeWorkLesson5.java
+++ src/main/java/homework/lesson5/MainHomeWorkLesson5.java
@@ -1,10 +1,9 @@
 package homework.lesson5;
 
 public class MainHomeWorkLesson5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ArrayClass arrayClass = new ArrayClass();
         System.out.println("Метод 1 выполнился за " + arrayClass.method1());
         System.out.println("Метод 2 выполнился за " + arrayClass.method2());
-        System.out.println("Метод 3 выполнился за " + arrayClass.method3());
     }
 }
